8e0646c48fedab4a91ca06e29ca287d0537c85cf
abrilsinflores <abril.perez@alumnos.upm.es>
2025-09-30 12:56:57 +0200

    Ejercicio 3 manejo d archivos

--- manejoDeArchivos/Ejercicio3.java
+++ manejoDeArchivos/Ejercicio3.java
@@ -1,5 +1,33 @@
 package manejoDeArchivos;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Ejercicio3 {
+	//Añada 3 línes de texto escritas por el usuario al final de un archivo.
+	public static void main(String[] args) {
+		String rutaArchivo = "C:\\Users\\Usuario\\OneDrive\\Desktop\\ALED\\prueba_lectura_datos.txt";
+		
+		try {
+			File f = new File(rutaArchivo);
+			FileOutputStream fos = new FileOutputStream(f,true);
+			PrintWriter pw = new PrintWriter(fos);
+			
+			Scanner sc = new Scanner(System.in);
+			String mensajeUsuario = sc.nextLine();	
+			
+			pw.println("\n"+mensajeUsuario);
+		
+			sc.close();
+			pw.close();
+			fos.close();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			System.out.println("no se ha podido ejecutar el main");
+		}
+	}
 
 }
